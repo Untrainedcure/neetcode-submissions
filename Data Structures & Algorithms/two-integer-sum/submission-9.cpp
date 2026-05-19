ab194c2d02db0067de4dce79f34fc6cf6fae6ee1
Soumyadip Paul <pal2soumyapal@gmail.com>
2026-05-19 22:57:39 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n =nums.size();
+        for(int i=0;i<n-1;i++) {
+            for(int j=0;j<n;j++) {
+                if(nums[i]+nums[j]==target) {
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+
+        
+    }
+};
