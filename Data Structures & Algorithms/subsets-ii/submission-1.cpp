841dde7a9098d4b60d2633073f25abc78ad5ebcb
Soumyadip Paul <pal2soumyapal@gmail.com>
2026-06-18 21:50:36 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void getSubsets(int id, vector<int>&nums , vector<int>&curr,vector<vector<int>>&ans) {
+        ans.push_back(curr);
+        for(int i=id;i<nums.size();i++) {
+            if(i!=id && nums[i]==nums[i-1]) {
+                continue;
+            }
+            curr.push_back(nums[i]);
+            getSubsets(i+1,nums,curr,ans);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        sort(nums.begin(),nums.end());
+        getSubsets(0,nums,curr,ans);
+        return ans;
+        
+    }
+};
